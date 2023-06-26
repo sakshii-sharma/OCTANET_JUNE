91a438c547012e48ef98309edda9014ce35afbc3
sakshii-sharma <94974861+sakshii-sharma@users.noreply.github.com>
2023-06-26 22:10:41 +0530

    Add files via upload

---
+++ TASK3/script.js
@@ -0,0 +1,28 @@
+// Get the elements by their class or tag names
+const leftNav = document.querySelector(".left");
+const searchInput = document.querySelector(".search");
+const profile = document.querySelector(".profile");
+const wishlist = document.querySelector(".wishlist");
+const bag = document.querySelector(".bag");
+
+// Add event listeners or perform any actions on the elements
+leftNav.addEventListener("click", function(event) {
+  // Handle click event on left navigation menu
+});
+
+searchInput.addEventListener("input", function(event) {
+  const searchValue = event.target.value;
+  // Perform search operation based on the input value
+});
+
+profile.addEventListener("click", function(event) {
+  // Handle click event on profile
+});
+
+wishlist.addEventListener("click", function(event) {
+  // Handle click event on wishlist
+});
+
+bag.addEventListener("click", function(event) {
+  // Handle click event on bag
+});
